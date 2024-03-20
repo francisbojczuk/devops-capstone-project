68f681cafdd87723f0ee00201befd9908a36eaa4
Francis Bojczuk <francisbojczuk@gmail.com>
2024-03-20 13:50:29 +0000

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,7 +137,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-        
+
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
@@ -150,6 +150,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -188,7 +189,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
+
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
